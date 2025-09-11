0194e7f2203be4fc0d92ba5feac6aa53fae4e13b
jagag0 <gagojakub@gmail.com>
2025-09-11 17:17:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name="Sudoku",
+setup(
+    name="pygame_sudoku",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=["pygame>=2.0.0"],
-    entry_points={"console_scripts": ["sudoku = src.main:main"]},
-    classifiers=["Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"],
-    python_requires=">=3.7")
+    install_requires=[
+        "pygame>=2.0.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "sudoku = main:main"
+        ]
+    },
+    python_requires=">=3.7",
+)
