8d9d2d5f6eeda27c8bfba792b057de7173d35fb2
jagag0 <gagojakub@gmail.com>
2025-09-18 15:53:47 +0200

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -17,23 +17,12 @@ def is_filled(grid):
         for col in range(9):
             if grid[row][col] == 0:
                 filled = False
-
     return filled
-
-def initialize_candidates_grid(grid):
-    '''Initializes 9*9 grid of cells listing potential candidates
-    that can be validly placed in each.'''
-    candidates = [[set() for _ in range(9)] for _ in range(9)]
-    for row in range(9):
-        for col in range(9):
-            if grid[row][col] == 0:
-                candidates[row][col] = {num for num in range(1, 10) if is_valid(grid, row, col, num)}
-    return candidates
-
 
 def copy_grid(grid):
     return [row.copy() for row in grid]
 
 def get_filled(grid):
     return {(r, c) for r in range(9) for c in range(9) if grid[r][c] != 0}
+
 
